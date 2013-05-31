a5349795e0db1f60886b07304f686110b2e9f7ce
mr.Shu <mr@shu.io>
2013-05-31 12:15:24 +0200

    bang replacing

--- data/js/popup.js
+++ data/js/popup.js
@@ -271,14 +271,23 @@ document.getElementById('icon_advanced').onclick = function(){
 
 function add_bang(bang) {
   var inp = document.getElementById('search_form_input_homepage');
+
+  var bang_regex = /\!\w+/;
+
   if (inp.value === '') {
-    inp.style.color = '#000';
     inp.value = bang + ' ';
     inp.focus();
   } else {
-    inp.value += bang;
-    search();
+    var found_bangs = bang_regex.exec(inp.value);
+    if (found_bangs.length > 0) {
+        inp.value = inp.value.replace(found_bangs[0], bang);
+    } else {
+        inp.value += bang;
+        search();
+    }
   }
+
+  console.log('and done');
 }
 
 function ducky_check(){
