0be8f8602b2eabfa06eeaa5277236f4d01f4d8a2
MariagraziaAlastra <maria@duckduckgo.com>
2017-01-09 14:10:21 -0500

    Selenium test - cleanup

--- selenium-test/test.js
+++ selenium-test/test.js
@@ -1,14 +1,11 @@
 var firefox = require('selenium-webdriver/firefox');
 var webdriver = require('selenium-webdriver');
-var test = require('selenium-webdriver/testing/assert');
+var assert = require('selenium-webdriver/testing/assert');
 
 var profile = new firefox.Profile();
 profile.addExtension('../build/duckduckgo_plus.xpi');
 
-//var binary = new firefox.Binary("/Applications/FirefoxNightly.app/Contents/MacOS/firefox-bin");
-var options = new firefox.Options()
-//	.setBinary(binary)
-	.setProfile(profile);
+var options = new firefox.Options().setProfile(profile);
 var driver = new firefox.Driver(options);
 
 var capabilities = webdriver.Capabilities.firefox();
@@ -21,4 +18,4 @@ var wd = new webdriver.Builder()
 
 
 wd.get("resource://jid1-zadieub7xozojw-at-jetpack/data/html/popup.html");
-test(driver.findElement(new webdriver.By('id','search_form_homepage')));
+assert(driver.findElement(new webdriver.By('id','search_form_homepage')));
