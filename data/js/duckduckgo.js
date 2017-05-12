e67b684bd1a0acc66fc03bc8e6c2a9bd572964e6
Brian Stoner <brianstoner@Brians-MacBook-Pro-2.local>
2017-05-11 20:35:18 -0400

    hide toolbar icon by default when flag is set on the DOM

--- data/js/duckduckgo.js
+++ data/js/duckduckgo.js
@@ -2,6 +2,10 @@
 function getATBParam() {
   return document.querySelector('html').getAttribute('data-atb');
 }
+function getHideToolbarIcon() {
+  return document.querySelector('html').getAttribute('data-hidetoolbaricon');
+}
 
 var atbParam = getATBParam();
-self.port.emit('loaded', atbParam);
+var hideToolbarIcon = getHideToolbarIcon();
+self.port.emit('loaded', atbParam, hideToolbarIcon);
