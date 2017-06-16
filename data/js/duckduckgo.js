b70cbc6e74f557dde74bcabe9500c38a874882d6
jdorweiler <jason@duckduckgo.com>
2017-06-16 15:17:38 -0600

    remove icon

--- data/js/duckduckgo.js
+++ data/js/duckduckgo.js
@@ -3,7 +3,7 @@ function getATBParam() {
   return document.querySelector('html').getAttribute('data-atb');
 }
 function getHideToolbarIcon() {
-  return document.querySelector('html').getAttribute('data-hidetoolbaricon');
+  return document.querySelector('html').getAttribute('data-hidetoolbarbutton');
 }
 
 var atbParam = getATBParam();
